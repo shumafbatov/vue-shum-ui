9c2dfcfd75a88043e8971cdbb5a33279a6bcc624
Shumaf B. <shumafbatov.dev@gmail.com>
2025-11-23 18:25:41 +0300

    fix: исправлена конфигурация ESLint и ошибки линтера

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,23 @@
 /* eslint-env node */
-require('@vue/tsconfig/tsconfig.json')
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/typescript/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  env: {
+    node: true,
+    es2022: true
   },
   rules: {
     'vue/multi-word-component-names': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
   }
 }
 
